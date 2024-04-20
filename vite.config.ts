d813f6597676472176e03a16bb3ae70923fb6b91
Felipe Apolinário <112521745+feapolina@users.noreply.github.com>
2024-04-20 20:59:48 -0300

    fix: fix vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/app-qrcode",
+  base: "/app-qrcode/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
